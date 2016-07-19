a34df99186f2bb21880854607abc059659ed212c
Ben Giamarino <giamarinob@gmail.com>
2016-07-19 16:10:23 +0000

    Add member migration and model

---
+++ app/models/member.rb
@@ -0,0 +1,5 @@
+class Member < ActiveRecord::Base
+	validates :name, presence: true
+	validates :email, presence: true, uniqueness: true
+	validates :password, presence: true, length: { minimum: 5 }
+end
