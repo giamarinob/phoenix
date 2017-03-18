bd475e7852f3978827a06aa1bbbcfa49b8266e69
Ben Giamarino <giamarinob@gmail.com>
2017-03-18 19:30:41 +0000

    Obscurity

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 Aws.config.update({
   region: 'us-west-1',
-  credentials: Aws::Credentials.new(ENV['AKIAJZBYGLFUN3K3RDDA'], ENV['Briefb6nB2xVAPtbSLGGJxTtsAGaaZIY7pJH1HNp']),
+  credentials: Aws::Credentials.new(ENV['S3_KEY'], ENV['S3_SECRET']),
 })
 
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['pclubassets'])
+S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   	config.fog_credentials = {
 	  	:provider              => 'AWS',
-	  	:aws_access_key_id     => "AKIAJZBYGLFUN3K3RDDA", #ENV['S3_KEY'],
-	  	:aws_secret_access_key => "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", #ENV['S3_SECRET']
+	  	:aws_access_key_id     => ENV['S3_KEY'],
+	  	:aws_secret_access_key => ENV['S3_SECRET']
 	  	:region                => "us-west-1"
 	  }
 
