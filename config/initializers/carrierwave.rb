4fa0109139ffc9be4b895a1eeb23fd895005f268
Ben Giamarino <giamarinob@gmail.com>
2017-03-22 04:37:41 +0000

    Add some logic to carrierwave to load local yaml file when not on production. Add some new copy to home page.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,16 @@
 CarrierWave.configure do |config|
-  	config.fog_credentials = {
-	  	:provider              => 'AWS',
-	  	:aws_access_key_id     => ENV['S3_KEY'],
-	  	:aws_secret_access_key => ENV['S3_SECRET'],
-	  	:region                => "us-west-1"
-	  }
 
-	  config.cache_dir     = "#{Rails.root}/tmp/uploads"
-	  config.fog_directory = ENV['S3_BUCKET']
+	if !Rails.env.production?
+		ENV = YAML.load_file("#{Rails.root.to_s}/config/application.yml")
+	end
+	
+	config.fog_credentials = {
+  	:provider              => 'AWS',
+  	:aws_access_key_id     => ENV['S3_KEY'],
+  	:aws_secret_access_key => ENV['S3_SECRET'],
+  	:region                => "us-west-1"
+  }
+
+  config.cache_dir     = "#{Rails.root}/tmp/uploads"
+  config.fog_directory = ENV['S3_BUCKET']
 end
